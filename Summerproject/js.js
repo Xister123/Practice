523a9092d4deb02460ee5c7a7fbcb9414cbb9a4b
Xister123 <159147067+Xister123@users.noreply.github.com>
2024-02-21 23:58:28 +0545

    Update js.js

--- Summerproject/js.js
+++ Summerproject/js.js
@@ -1,26 +1,27 @@
 <script>  
-function validateform(){  
-var doc_username=document.myform.name.value;  
-var password=document.myform.password.value;  
-  
-if ( doc_username=="" ||  doc_username==""){  
-  alert("Name can't be blank");  
-  return false;  
-}else if(password.length<6){  
-  alert("Password must be at least 6 characters long.");  
-  return false;  
-  }  
+function validateDoctorForm(){  
+    var doc_username = document.getElementsByName("doc_username")[0].value;  
+    var password = document.getElementsByName("doc_password")[0].value;  
+
+    if (doc_username == "" || doc_username == null){  
+        alert("Username can't be blank");  
+        return false;  
+    } else if(password.length < 6){  
+        alert("Password must be at least 6 characters long.");  
+        return false;  
+    }  
 }  
-function validateform(){  
-var pat_username=document.myform.name.value;  
-var password=document.myform.password.value;  
-  
-if ( pat_username==null ||  pat_username==""){  
-  alert("Name can't be blank");  
-  return false;  
-}else if(password.length<6){  
-  alert("Password must be at least 6 characters long.");  
-  return false;  
-  }  
+
+function validatePatientForm(){  
+    var pat_username = document.getElementsByName("pat_username")[0].value;  
+    var password = document.getElementsByName("pat_password")[0].value;  
+
+    if (pat_username == "" || pat_username == null){  
+        alert("Username can't be blank");  
+        return false;  
+    } else if(password.length < 6){  
+        alert("Password must be at least 6 characters long.");  
+        return false;  
+    }  
 }  
-</script> 
+</script>
